b6031f0057739ce564cfa718e4dc8d8268c9210f
basit011 <156387950+basit011@users.noreply.github.com>
2024-10-11 20:58:21 -0400

    assignment1 updated db

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
-const connectDb = async() => {
+const connectDb = async () => {
     try {
-        await mongoose.connect("mongodb+srv://basit17ali:UiAg9RUITQMnOFES@seriescluster.7fcxk.mongodb.net/comp3123_assignment1");
-            console.log("MongoDB Connected...");
-    }catch(error){
+        
+        await mongoose.connect(process.env.DB_URI);
+        console.log("MongoDB Connected...");
+    } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
         process.exit(1);
     }
-}
+};
 
-module.exports = connectDb
+module.exports = connectDb;
